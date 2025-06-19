10fa5f8d3eb585777d8132ab80098307048301bc
MikeShark67 <oumam504@gmail.com>
2025-06-19 11:33:39 +0300

    added another question

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let call = require(`readline`)
+let call = require(`readline`);
 let soma = call.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -7,7 +7,9 @@ soma.question("What's your favourite City?", Kisumu => {
   console.log(`Fav City: ${Kisumu}`)
   
 soma.question("What's your favourite Show?", Kamum=> {
-  console.log(`Fav City: ${Kamum}`);
+  console.log(`Fav City: ${Kamum}`)
+  soma.question("Whats your favourite team?", Arsenal => {
+    console.log(`Fav Team: ${Arsenal}`)
   soma.close(2);
-});});
+});});});
 
